ca3666662daa965ab5a31a2bc72e174e350b4887
IOAN005 <nellbovinca@gmail.com>
2023-12-22 19:40:03 +0100

    maven1

--- src/com/corenetworks/persistencia/AccesoFunciones.java
+++ src/com/corenetworks/persistencia/AccesoFunciones.java
@@ -1,2 +1,49 @@
-package com.corenetworks.persistencia;public class AccesoFunciones {
+package com.corenetworks.persistencia;
+
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccesoFunciones extends Conexion{
+    public int probarFunction() throws SQLException, ClassNotFoundException {
+        //1. Declaracion de variable
+        CallableStatement sentencia = null;
+        ResultSet rejilla =null;
+        String sql ="select public.cantidades_existencia()";
+        int resultado =0;
+        //2.Abrir la conexion
+        abrirConexion();
+
+        //3.Crear el Statment
+        sentencia= miConexion.prepareCall(sql);
+
+        //4.Ejecutar el statment
+        rejilla= sentencia.executeQuery();
+        if(rejilla.next()){
+            resultado=rejilla.getInt("cantidades_existencia");
+        }
+        //.5 devolver el resultado
+        return resultado;
+    }
+    public String probarFunctionConParametro(int id) throws SQLException, ClassNotFoundException {
+        //1. Declaracion de variable
+        CallableStatement sentencia = null;
+        ResultSet rejilla = null;
+        String sql = "select public.nombre_producto(?)";
+        String resultado = null;
+        //2.Abrir la conexion
+        abrirConexion();
+
+        //3.Crear el Statment
+        sentencia = miConexion.prepareCall(sql);
+        sentencia.setInt(1,id);
+
+        //4.Ejecutar el statment
+        rejilla = sentencia.executeQuery();
+        if (rejilla.next()) {
+            resultado = rejilla.getString("nombre_producto");
+        }
+        //.5 devolver el resultado
+        return resultado;
+    }
 }
--- src/com/corenetworks/presentacion/ProbarFuncion.java
+++ src/com/corenetworks/presentacion/ProbarFuncion.java
@@ -1,2 +1,27 @@
-package com.corenetworks.presentacion;public class ProbarFuncion {
+package com.corenetworks.presentacion;
+
+import com.corenetworks.persistencia.AccesoFunciones;
+
+import java.sql.SQLException;
+
+public class ProbarFuncion {
+    public static void main(String[] args) {
+        AccesoFunciones af1= new AccesoFunciones();
+        try {
+            System.out.println("Cantidad en existencia   " +af1.probarFunction());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+        try {
+            System.out.println("nombre del producto " + af1.probarFunctionConParametro(10));
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+
 }
